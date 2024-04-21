1f1b02d9cb53115391431a54f5220878c2663de4
jonib <jonibpereira@gmail.com>
2024-04-21 04:02:12 +0100

    lb fix + profile buttons

--- app/src/main/java/com/example/GoalGuide/ProfileActivity.java
+++ app/src/main/java/com/example/GoalGuide/ProfileActivity.java
@@ -2,6 +2,9 @@ package com.example.GoalGuide;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,7 @@ import com.example.test.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private Button save, editPfp, back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         if (getSupportActionBar() != null) {
@@ -22,5 +26,20 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+
+        back = findViewById(R.id.back_profile);
+        save = findViewById(R.id.save_profile);
+
+
+        back.setOnClickListener(v -> finish());
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ProfileActivity.this, "Saved changes", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
+
+
 }
