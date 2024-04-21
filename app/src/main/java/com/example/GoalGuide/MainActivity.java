3003b6c49157dfee92183ad1ea5a133c4913aadf
jonib <jonibpereira@gmail.com>
2024-04-21 01:43:53 +0100

    profile

--- app/src/main/java/com/example/GoalGuide/MainActivity.java
+++ app/src/main/java/com/example/GoalGuide/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.GoalGuide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.test.R;
 import com.example.test.databinding.ActivityMainBinding;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private Button profileButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
 
         View trademarkDark = findViewById(R.id.trademark_dark);
 
+        profileButton = findViewById(R.id.profile_button);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -42,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.trademarkDark.navView, navController);
     }
+
+
+
+
 
 
 }
